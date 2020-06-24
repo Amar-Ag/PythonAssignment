c70993fc13ae65f26177943075cd0b8ec4c7373c
Amar Agrawal <amaragrawal48@gmail.com>
2020-06-24 23:44:23 +0545

    DataType 5-10

--- DataTypes1.py
+++ DataTypes1.py
@@ -1,40 +1,40 @@
-# """
-# 1) Python program to count the number of characters (character
-# frequency) in a string. Sample String : google.com
-# """
-# sampleString = str(input("Enter a string:")) or "google.com"
-# sampleDict = {}
-# for i in sampleString:
-#     keys = sampleDict.keys()
-#     if i in keys:
-#         sampleDict[i] += 1
-#     else:
-#         sampleDict[i] = 1
-# print("Characters and their occurences in given string = ", sampleDict)
-#
-# """
-# 2) Python program to get a string made of the first 2 and the last 2 chars
-# from a given a string. If the string length is less than 2, return instead of the
-# empty string.
-# """
-# sampleString2 = input("Enter a string:") or "Python"
-# stringLength = len(sampleString2)
-# newString = ""
-# if stringLength >= 2:
-#     firstLetters = sampleString2[0:2]
-#     lastLetters = sampleString2[-2:]
-#     newString = firstLetters + lastLetters
-# else:
-#     newString = "Empty String"
-# print(newString)
-#
-# """
-# 3) Python program to get a string from a given string where all
-# occurrences of its first char have been changed to '$', except the first char itself.
-# """
-# sampleString3 = str(input("Enter a string:")) or "restart"
-# firstLetter = sampleString3[0]
-# print(firstLetter+sampleString3[1:].replace(firstLetter, "$",))
+"""
+1) Python program to count the number of characters (character
+frequency) in a string. Sample String : google.com
+"""
+sampleString = str(input("Enter a string:")) or "google.com"
+sampleDict = {}
+for i in sampleString:
+    keys = sampleDict.keys()
+    if i in keys:
+        sampleDict[i] += 1
+    else:
+        sampleDict[i] = 1
+print("Characters and their occurences in given string = ", sampleDict)
+
+"""
+2) Python program to get a string made of the first 2 and the last 2 chars
+from a given a string. If the string length is less than 2, return instead of the
+empty string.
+"""
+sampleString2 = str(input("Enter a string:")) or "Python"
+stringLength = len(sampleString2)
+newString = ""
+if stringLength >= 2:
+    firstLetters = sampleString2[0:2]
+    lastLetters = sampleString2[-2:]
+    newString = firstLetters + lastLetters
+else:
+    newString = "Empty String"
+print(newString)
+
+"""
+3) Python program to get a string from a given string where all
+occurrences of its first char have been changed to '$', except the first char itself.
+"""
+sampleString3 = str(input("Enter a string:")) or "restart"
+firstLetter = sampleString3[0]
+print(firstLetter+sampleString3[1:].replace(firstLetter, "$",))
 
 """
 4) Python program to get a single string from two given strings, separated
@@ -51,7 +51,7 @@ print(outputString1+" "+outputString2)
 be at least 3). If the given string already ends with 'ing' then add 'ly' instead. If the
 string length of the given string is less than 3, leave it unchanged.
 """
-sampleString4 = input("Enter a string:") or "string"
+sampleString4 = str(input("Enter a string:")) or "string"
 sampleLength = len(sampleString4)
 if sampleLength >= 3:
     if sampleString4[-3:]=='ing':
---
+++ DataTypes2.py
@@ -0,0 +1,63 @@
+# """
+# 6) Python program to find the first appearance of the substring 'not' and
+# 'poor' from a given string, if 'not' follows the 'poor', replace the whole 'not'...'poor'
+# substring with 'good'. Return the resulting string.
+# """
+# sampleInput = str(input("Enter any string:")) or "The lyrics is not that poor!"
+# wordOne = "not"
+# wordTwo = "poor"
+# if wordOne in sampleInput:
+#     notIndex = sampleInput.index(wordOne, 1)
+#     if wordTwo in sampleInput:
+#         poorIndex = sampleInput.index(wordTwo, 1)
+#         if notIndex < poorIndex:
+#             print(sampleInput[:notIndex] + "good" + sampleInput[poorIndex + 4:])
+#     else:
+#         print("The string was not changed:", sampleInput)
+# else:
+#     print("The string was not changed:", sampleInput)
+#
+# """
+# 7) Python function that takes a list of words and returns the length of the
+# longest one.
+# """
+#
+#
+# def longestWord(wordList):
+#     largest = 0;
+#     for i in wordList:
+#         wordLength = len(i)
+#         if wordLength > largest:
+#             largest = wordLength
+#     return largest
+#
+#
+# sampleList = ['This', 'is', 'an', 'assignment', 'for', 'learning', 'Python']
+# print("Largest length in the supplied list is:", longestWord(sampleList))
+
+# """
+# 8) Python program to remove the n th index character from a nonempty string.
+# """
+# sampleString = str(input("Enter a string:")) or "Python"
+# indexToRemove = int(input("Enter the index that you want to remove:"))
+# print(sampleString[:indexToRemove] + sampleString[indexToRemove + 1:])
+
+"""
+9) Python program to change a given string to a new string where the first
+and last chars have been exchanged.
+"""
+sampleString2 = str(input("Enter a string:")) or "Python"
+newString = sampleString2[-1].upper() + sampleString2[1:-1] + sampleString2[0].lower()
+print(newString)
+
+"""
+10) Python program to remove the characters which have odd index
+values of a given string.
+"""
+sampleString3 = str(input("Enter a string:")) or "Python"
+evenString = ""
+for i in sampleString3:
+    index = sampleString3.find(i)
+    if index % 2 == 0:
+        evenString += i
+print(evenString)
