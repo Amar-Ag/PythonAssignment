9bc4aa54950a0137fb4b8c22393b06026ca1260d
Amar Agrawal <amaragrawal48@gmail.com>
2020-06-26 00:54:01 +0545

    DataType 11-15

--- DataTypes3.py
+++ DataTypes3.py
@@ -1,37 +1,37 @@
-# """
-# 11) Python program to count the occurrences of each word in a given
-# sentence.
-# """
-# sampleSentence = str(input("Enter a sentence:")) or "This is a sample Python sentence for Python assignment"
-# sentenceWords = sampleSentence.split(" ")
-# countDict = {}
-# print(sentenceWords)
-# for word in sentenceWords:
-#     keys = countDict.keys()
-#     if word in keys:
-#         countDict[word] += 1
-#     else:
-#         countDict[word] = 1
-# print(countDict)
-#
-# """
-# 12) Python script that takes input from the user and displays that input
-# back in upper and lower cases.
-# """
-# sampleInput = str(input("Enter sample input:")) or "Sample Input"
-# print("Upper Case:", sampleInput.upper())
-# print("Lower Case:", sampleInput.lower())
-#
-# """
-# 13) Python program that accepts a comma separated sequence of words
-# as input and prints the unique words in sorted form (alphanumerically).
-# """
-# sampleWords = str(input("Enter comma separated sequence of words")) or "red, white, black, red, green, black"
-# sampleWords = sampleWords.split(',')
-# sampleWords = list(
-#     set(sampleWords))  # Converting to set 1st to keep the unique elements and converting the set to a list
-# print(",".join(sorted(sampleWords)))  # Sorting the list alphanumerically and then joining each element with a comma
-# # output includes ' red' and 'red' as different items due to the space ahead
+"""
+11) Python program to count the occurrences of each word in a given
+sentence.
+"""
+sampleSentence = str(input("Enter a sentence:")) or "This is a sample Python sentence for Python assignment"
+sentenceWords = sampleSentence.split(" ")
+countDict = {}
+print(sentenceWords)
+for word in sentenceWords:
+    keys = countDict.keys()
+    if word in keys:
+        countDict[word] += 1
+    else:
+        countDict[word] = 1
+print(countDict)
+
+"""
+12) Python script that takes input from the user and displays that input
+back in upper and lower cases.
+"""
+sampleInput = str(input("Enter sample input:")) or "Sample Input"
+print("Upper Case:", sampleInput.upper())
+print("Lower Case:", sampleInput.lower())
+
+"""
+13) Python program that accepts a comma separated sequence of words
+as input and prints the unique words in sorted form (alphanumerically).
+"""
+sampleWords = str(input("Enter comma separated sequence of words")) or "red, white, black, red, green, black"
+sampleWords = sampleWords.split(',')
+sampleWords = list(
+    set(sampleWords))  # Converting to set 1st to keep the unique elements and converting the set to a list
+print(",".join(sorted(sampleWords)))  # Sorting the list alphanumerically and then joining each element with a comma
+# output includes ' red' and 'red' as different items due to the space ahead
 
 """
 14) Python function to create the HTML string with tags around the
